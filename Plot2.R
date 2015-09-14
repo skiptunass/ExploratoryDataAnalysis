cb146e04fe282ebf0c0f363ec1cf11cf55376a44
Steve Skiptunas <steve.skiptunas@gmail.com>
2015-09-13 21:24:36 -0400

    Create Plot2.R

---
+++ Plot2.R
@@ -0,0 +1,21 @@
+##############################################################################
+# Plot 2
+# set working directory 
+setwd("C:/Users/Steve/Desktop/Coursera/Exploratory_Data_Analysis/Project_1")
+
+# make sure the plots folder exists
+if (!file.exists('plots')) {
+    dir.create('plots')
+}
+
+# load data
+source('scripts/get_and_clean_data.R')
+
+# open device
+png(filename='plots/plot2.png',width=480,height=480,units='px')
+
+# plot data
+plot(power.consumption$DateTime,power.consumption$GlobalActivePower,ylab='Global Active Power (kilowatts)', xlab='', type='l')
+
+# close device
+x<-dev.off()
